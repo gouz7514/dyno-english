300bc3f9df31b148ffcee7d4fb1299a7bbff57d2
gouz7514 <gouz7514@gmail.com>
2023-06-21 19:16:44 +0900

    Update : main page lottie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,42 @@
 'use client'
 
+import styled from 'styled-components'
+
+import AnimationDynoEnglish from '../../public/lottie/lottie-dyno-english.json'
+import LottieAnimation from './components/Lottie'
+
+const MainStyle = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  padding: 1rem;
+  min-height: calc(100vh - var(--height-header));
+
+  .main-text {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`
+
 export default function Home() {
   return (
     <main>
-      <h1>
-        메인 페이지
-      </h1>
+      <div>
+        <MainStyle className="main-container">
+          <LottieAnimation json={AnimationDynoEnglish} />
+          <div className='main-text'>
+            <h2>
+              다이노 영어에 오신 것을 환영해요!
+            </h2>
+            <h2>
+              오른쪽 위 메뉴를 둘러보세요!
+            </h2>
+          </div>
+        </MainStyle>
+      </div>
     </main>
   )
 }
