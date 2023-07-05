bb3f867ee06de8ff0c8cbf18e128f0e18c6a925b
gouz7514 <gouz7514@gmail.com>
2023-07-05 20:47:45 +0900

    Add : calendar

--- src/app/study/table/page.tsx
+++ src/app/study/table/page.tsx
@@ -1,7 +1,88 @@
+'use client'
+
+import styled from 'styled-components'
+import { useEffect } from "react"
+
+import Calendar from "@toast-ui/calendar"
+import '@toast-ui/calendar/dist/toastui-calendar.min.css';
+
+const CalendarContainer = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  margin-top: 24px;
+
+  .calendar-title {
+    margin-bottom: 12px;
+  }
+`
+
 export default function StudyTable() {
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+
+  useEffect(() => {
+    const calendar = new Calendar('#claendar', {
+      defaultView: 'week',
+      week: {
+        dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+        taskView: false
+      },
+      month: {
+        dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+      },
+      calendars: [
+        {
+          id: 'cal1',
+          name: 'Personal',
+          backgroundColor: '#03bd9e',
+        },
+        {
+          id: 'cal2',
+          name: 'Work',
+          backgroundColor: '#00a9ff',
+        },
+      ],
+    })
+
+    calendar.createEvents([
+      {
+        id: '1',
+        calendarId: '1',
+        title: '수업 1',
+        category: 'time',
+        dueDateClass: '',
+        start: '2023-07-05T14:00:00+09:00',
+        end: '2023-07-05T14:50:00+09:00',
+        backgroundColor: '#ffbb3b',
+      },
+      {
+        id: '2',
+        calendarId: '1',
+        title: '수업 2',
+        category: 'time',
+        dueDateClass: '',
+        start: '2023-07-05T15:00:00+09:00',
+        end: '2023-07-05T15:50:00+09:00',
+        backgroundColor: '#009222',
+      },
+    ]);
+  }, [])
+  
   return (
-    <>
-      <h1>시간표 페이지</h1>
-    </>
+    <CalendarContainer>
+      <div className="calendar-title">
+        <h1>
+          {
+            `${year}년 ${month}월`
+          }
+        </h1>
+      </div>
+      <div id="claendar" style={{ 'height': '100vh', 'width': '100%' }}></div>
+    </CalendarContainer>
   )
 }
