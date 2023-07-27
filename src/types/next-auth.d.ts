bc54e24bd69c44a78e7c0895f08e7c9bf374edfc
gouz7514 <gouz7514@gmail.com>
2023-07-27 17:24:32 +0900

    Update : firebase auth 연결, 회원정보, 로그인 페이지 추가

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,14 +1,16 @@
 import NextAuth from "next-auth"
 
 declare module "next-auth" {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
   interface Session {
     user: {
-      /** The user's postal address. */
       name: string,
-      userId: string
-    }
+      userId: string,
+      isStaff: boolean,
+    },
+    classInfo: {
+      id: number,
+      name: string,
+      curriculum: object
+    },
   }
 }
