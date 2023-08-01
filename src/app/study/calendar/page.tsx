d6c36b5fb5a7ac14ea7b851cc958cbefc08fda3a
gouz7514 <gouz7514@gmail.com>
2023-08-01 19:04:37 +0900

    Test : react big calendar

--- src/app/study/calendar/page.tsx
+++ src/app/study/calendar/page.tsx
@@ -1,39 +1,77 @@
 'use client'
 
 import styled from 'styled-components'
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
 
 import dynamic from 'next/dynamic'
 
-import { CalendarProps } from '@/types/types'
+import { Calendar, momentLocalizer, View } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import moment from 'moment'
+import 'moment/locale/ko'
 
 const CalendarContainer = styled.div`
-  width: 100%;
-  max-width: 1024px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 0 auto;
-  margin-top: 24px;
+  // width: 100%;
+  // max-width: 1024px;
+  // display: flex;
+  // flex-direction: column;
+  // align-items: center;
+  // margin: 0 auto;
+  // margin-top: 24px;
 
-  .toastui-calendar-allday {
+  // .toastui-calendar-allday {
+  //   display: none !important;
+  // }
+
+  // .toastui-calendar-day-name__date {
+  //   font-size: 16px !important;
+  // }
+
+  // .toastui-calendar-day-name-container {
+  //   margin-left: 48px !important;
+  // }
+
+  // .toastui-calendar-timegrid-time-column {
+  //   width: 48px !important;
+  // }
+
+  // .toastui-calendar-columns {
+  //   left: 48px !important;
+  // }
+
+  .rbc-header {
+    border-bottom: none;
+  }
+
+  .rbc-event-label,
+  .rbc-allday-cell {
     display: none !important;
   }
 
-  .toastui-calendar-day-name__date {
-    font-size: 16px !important;
+  .rbc-label {
+    font-size: 12px;
   }
 
-  .toastui-calendar-day-name-container {
-    margin-left: 48px !important;
-  }
+  .rbc-events-container {
+    margin-right: 0 !important;
 
-  .toastui-calendar-timegrid-time-column {
-    width: 48px !important;
-  }
+    .rbc-event {
+      box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+      background-color: beige;
+      color: black;
+      font-size: 12px;
+      padding: 2px;
+      border: none;
+      display: flex;
+      align-items: center;
+      justify-content: center;
 
-  .toastui-calendar-columns {
-    left: 48px !important;
+      .rbc-event-content {
+        height: auto;
+        text-align: center;
+        flex: 0 1 auto;
+      }
+    }
   }
 `
 
@@ -50,58 +88,77 @@ CalendarRef.displayName = 'CalendarRef'
 
 export default function StudyCalendar() {
   const date = new Date()
+  const [calendarView, setCalendarView] = useState<View>('week')
   const year = date.getFullYear()
   const month = date.getMonth() + 1
 
-  const cal = useRef(null)
-
-  const calendarProps: CalendarProps = {
-    view: 'week',
-    week: {
-      dayNames: ['일', '월', '화', '수', '목', '금', '토'],
-      taskView: false,
-    },
-    month: {
-      dayNames: ['일', '월', '화', '수', '목', '금', '토'],
-    },
-    calendars: [
-      {
-        id: 'cal1',
-        name: 'Personal',
-        backgroundColor: '#03bd9e',
-      },
-      {
-        id: 'cal2',
-        name: 'Work',
-        backgroundColor: '#00a9ff',
-      },
-    ],
-    events: []
-  }
-
-  const calendarEvents = [
+  const events = [
     {
-      id: '1',
-      calendarId: '1',
-      title: '수업 1',
-      category: 'time',
-      dueDateClass: '',
-      start: '2023-07-05T14:00:00+09:00',
-      end: '2023-07-05T14:50:00+09:00',
-      backgroundColor: '#ffbb3b',
-    }
+      title: '파닉스-K (1)',
+      start: moment('2023-08-03T13:00:00+09:00').toDate(),
+      end: moment('2023-08-03T13:50:00+09:00').toDate(),
+      bgColor: 'lightblue'
+    },
+    {
+      title: '원서리딩',
+      start: moment('2023-08-03T14:00:00+09:00').toDate(),
+      end: moment('2023-08-03T14:50:00+09:00').toDate(),
+      bgColor: 'lightgreen'
+    },
   ]
 
-  calendarProps.events = calendarEvents
+  const localizer = momentLocalizer(moment)
 
   return (
     <CalendarContainer>
-      <div className="calendar-title">
-        <h1>
-          {year}년 {month}월
-        </h1>
-      </div>
-      <CalendarRef ref={cal} {...calendarProps} />
+      <Calendar
+        view={calendarView}
+        views={['month', 'week']}
+        onView={(view) => {
+          setCalendarView(view)
+        }}
+        localizer={localizer}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 800 }}
+        events={events}
+        formats={{
+          timeGutterFormat: (date) => {
+            return moment(date).format('H')
+          }
+        }}
+        eventPropGetter={(event) => {
+          let newStyle = {
+            backgroundColor: event.bgColor,
+          }
+
+          return {
+            style: newStyle
+          }
+        }}
+        dayPropGetter={(date) => {
+          if (date.getDay() === 0) {
+            return {
+              className: 'sunday'
+            }
+          }
+          return {}
+        }}
+        components={{
+          week: {
+            header: ({ date }) => {
+              return (
+                <div className="rbc-day-bg">
+                  <div className="rbc-header">
+                    <span className="rbc-label">{moment(date).format('D')}</span>
+                    <span className="rbc-label">{moment(date).format('ddd')}</span>
+                  </div>
+                </div>
+              )
+            }
+          }
+        }}
+      />
     </CalendarContainer>
   )
 }
