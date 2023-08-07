43cd504a0fd266a4a42a6149a7adecdf052ce000
gouz7514 <gouz7514@gmail.com>
2023-08-07 18:48:50 +0900

    Update : 전화번호 ayto hyphen 처리

--- src/app/profile/account/page.tsx
+++ src/app/profile/account/page.tsx
@@ -255,9 +255,12 @@ export default function ProfilePage() {
   }, [session, router, status, getUserInfo])
 
   const handleChangePhone = (e: any) => {
+    const autoHyphenValue = e.target.value.replace(/[^0-9]/g, '')
+    .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3").replace(/(\-{1,2})$/g, "");
+
     setUser({
       ...user,
-      phone: e.target.value
+      phone: autoHyphenValue
     })
   }
 
