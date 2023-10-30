5cec12b03787aa1d0bc1fcc4dbdceed69b255e93
gouz7514 <gouz7514@gmail.com>
2023-10-30 15:42:14 +0900

    Chore : 커리큘럼, 공지사항 삭제, 수정 버튼 위치 바꿈

--- src/app/components/Organism/AdminCurriculumForm.tsx
+++ src/app/components/Organism/AdminCurriculumForm.tsx
@@ -306,12 +306,6 @@ export default function AdminCurriculumForm({ isEdit }: AdminCurriculumFormProps
                 }
               </div>
               <div className="button-container">
-                <Button
-                  onClick={handleSubmit}
-                  disabled={curriculumName === '' || submitting}
-                >
-                  { isEdit ? '수정하기' : '추가하기' }
-                </Button>
                 {
                   isEdit && (
                     <Button
@@ -323,6 +317,12 @@ export default function AdminCurriculumForm({ isEdit }: AdminCurriculumFormProps
                     </Button>
                   )
                 }
+                <Button
+                  onClick={handleSubmit}
+                  disabled={curriculumName === '' || submitting}
+                >
+                  { isEdit ? '수정하기' : '추가하기' }
+                </Button>
               </div>
             </AdminCurriculumFormStyle>
           )
