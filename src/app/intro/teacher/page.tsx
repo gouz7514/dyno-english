7524e5574da8a30e4b594fef5f1029689cbde8c9
gouz7514 <gouz7514@gmail.com>
2023-11-07 13:24:57 +0900

    Update : 선생님 소개 페이지

--- src/app/intro/teacher/page.tsx
+++ src/app/intro/teacher/page.tsx
@@ -1,9 +1,120 @@
 'use client'
 
+import Image from "next/image"
 import { useState } from "react"
+import styled from "styled-components"
 
-import { ArticleStyle } from "@/app/styles/styles"
-import Divider from "@/app/components/Atom/Divider"
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Pagination } from "swiper"
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+
+import ImageProfilePicture from "../../../../public/images/intro/teacher/dyno-profile-picture.webp"
+
+SwiperCore.use([Pagination])
+
+const IntroTeacherStyle = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  height: calc(100vh - var(--height-header) - var(--height-footer));
+  padding: 24px 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  margin: 0 auto;
+
+  .profile-container {
+    width: 100%;
+    max-width: 400px;
+
+    .profile-picture {
+      border-radius: 12px;
+    }
+
+    @media screen and (max-width: 450px) {
+      max-width: 400px;
+    }
+  }
+
+  @media screen and (max-width: 850px) {
+    flex-direction: column;
+    align-items: center;
+    height: auto;
+  }
+
+  @media screen and (max-width: 450px) {
+    justify-content: start;
+  }
+`
+
+const IntroTeacherSwiper = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 400px;
+
+  .swiper {
+    position: relative;
+
+    .swiper-slide {
+      padding: 2px;
+      border-radius: 12px;
+
+      .swiper-teacher {
+        position: relative;
+        height: 500px;
+        padding: 12px 18px;
+        border-radius: 12px;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+        display: flex;
+        gap: 12px;
+        flex-direction: column;
+        touch-action: pan-y;
+        color: white;
+
+        &.first {
+          background: radial-gradient(at 90% 90%, rgb(255, 114, 58), rgb(255, 80, 110));
+        }
+
+        &.second {
+          background: radial-gradient(ellipse farthest-corner at 90% 90%, #00e244, #00b48e);
+        }
+
+        &.third {
+          background: radial-gradient(ellipse farthest-corner at 90% 90%, #882ef0, rgba(80, 115, 255, 0.8));
+        }
+
+        .swiper-title {
+          font-size: 24px;
+          font-weight: bold;
+        }
+
+        .swiper-content {
+          font-size: 16px;
+          word-break: keep-all;
+          white-space: pre-line;
+
+          p {
+            margin-bottom: 12px;
+            line-height: 1.5;
+            font-weight: 500;
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 850px) {
+    margin-top: 12px;
+    height: 500px;
+  }
+
+  @media screen and (max-width: 450px) {
+    max-width: 400px;
+  }
+`
 
 export default function IntroTeacher() {
   const [isToggled, setIsToggled] = useState<{
@@ -24,311 +135,107 @@ export default function IntroTeacher() {
   }
 
   return (
-    <ArticleStyle>
-      <div className="container">
-        <div className="article-title">
-          선생님 소개
-        </div>
-        <div className="article-content">
-          <p>
-            안녕하세요 :)
-          </p>
-          <p>
-            다이노 영어의
-          </p>
-          <p>
-            다이노 선생님입니다.
-          </p>
-          <br />
-          <p>
-            이번 포스팅에서는
-          </p>
-          <p>
-            제 소개를 드리려고 해요.
-          </p>
-          <br />
-          <p>
-            그동안 제가 걸어온 길을 
-          </p>
-          <p>
-            크게 세 부분으로 나눠
-          </p>
-          <p>
-            글을 써보도록 하겠습니다.
-          </p>
-          <Divider short />
-          <div className="article-section">
-            <div className="section-title toggle" onClick={() => onClickToggle('first')}>
-              <div className="section-title-text">
-                1. 미국 초·중학교 졸업 <div className="icon-click"></div>
-              </div>
-            </div>
-            {
-              isToggled.first && (
-                <div className="section-content">
-                  <p>
-                    저는 초등학교 4학년 때
-                  </p>
-                  <p>
-                    부모님과 함께 
-                  </p>
-                  <p>
-                    미국으로 갔습니다.
-                  </p>
-                  <br />
-                  <p>
-                    캘리포니아 주,
-                  </p>
-                  <p>
-                    Rancho Cucamonga 라는
-                  </p>
-                  <p>
-                    작은 동네에서
-                  </p>
-                  <p>
-                    John L. Golden Elementary School 과
-                  </p>
-                  <p>
-                    Day Creek Intermediate School 을
-                  </p>
-                  <p>
-                    졸업했어요.
-                  </p>
-                  <br />
-                  <p>
-                    미국 학교를 다니면서
-                  </p>
-                  <p>
-                    경험하고 배운 것들을
-                  </p>
-                  <p>
-                    늘 제 수업에 적용하고
-                  </p>
-                  <p>
-                    학부모님과 나누고자
-                  </p>
-                  <p>
-                    노력하고 있습니다.
-                  </p> 
-                </div>
-              )
-            }
-          </div>
-          <div className="article-section">
-            <div className="section-title toggle" onClick={() => onClickToggle('second')}>
-              <div className="section-title-text">
-                2. 영문학 전공 · 교직이수 <div className="icon-click"></div>
-              </div>
-            </div>
-            {
-              isToggled.second && (
-                <div className="section-content">
-                  <p>
-                  저는 동국대학교에서
-                  </p>
-                  <p>
-                  영어영문을 전공하고
-                  </p>
-                  <p>
-                  교직 과정을 이수했습니다.
-                  </p>
-                  <br />
-                  <p>
-                    대학 생활을 하면서
-                  </p>
-                  <p>
-                    전공 지식을 
-                  </p>
-                  <p>
-                    쌓는 것도 중요했지만,
-                  </p>
-                  <p>
-                    무엇보다 값진 경험은
-                  </p>
-                  <p>
-                    교육 봉사 활동이었어요.
-                  </p>
-                  <br />
-                  <p>
-                    교육 봉사를 하면서
-                  </p>
-                  <p>
-                    저는 교육이 단지 지식을 
-                  </p>
-                  <p>
-                    전달하는 게 아니라는 걸 
-                  </p>
-                  <p>
-                    알게 되었습니다.
-                  </p>
-                  <br />
-                  <p>
-                    저에게 교육은
-                  </p>
-                  <p>
-                    사람과 사람 간의 
-                  </p>
-                  <p>
-                    만남이었고,
-                  </p>
-                  <br />
-                  <p>
-                    이러한 만남을 통해
-                  </p>
-                  <p>
-                    서로의 세계를 이해하고 
-                  </p>
-                  <p>
-                    나누며 더 넓혀갈 수 
-                  </p>
-                  <p>
-                    있다는 것을 배웠어요.
-                  </p>
-                  <br />
-                  <p>
-                    아이들은 저에게 가장 큰 
-                  </p>
-                  <p>
-                    자극과 에너지를 
-                  </p>
-                  <p>
-                    주는 존재입니다.
-                  </p>
-                  <br />
-                  <p>
-                    그리고 아이들에게도
-                  </p>
-                  <p>
-                    늘 긍정적인 영향을 주는
-                  </p>
-                  <p>
-                    선생님으로 남고 싶어요.
-                  </p>
-                </div>
-              )
-            }
-          </div>
-          <div className="article-section">
-            <div className="section-title toggle" onClick={() => onClickToggle('third')}>
-              <div className="section-title-text">
-                3. 다양하고 실질적인 경험 <div className="icon-click"></div>
-              </div>
-            </div>
-            {
-              isToggled.third && (
-                <div className="section-content">
-                  <p>
-                    다이노 영어를
-                  </p>
-                  <p>
-                    시작하기까지 
-                  </p>
-                  <p>
-                    긴 여정이 있었습니다.
-                  </p>
-                  <br />
-                  <p>
-                    대학을 졸업한 후,
-                  </p>
-                  <p>
-                    임용 시험 준비부터
-                  </p>
-                  <p>
-                    영어 유치원과 학원
-                  </p>
-                  <p>
-                    그리고 사무직까지
-                  </p>
-                  <p>
-                    다양한 경험을 했습니다.
-                  </p>
-                  <br />
-                  <p>
-                    돌고 돌아 
-                  </p>
-                  <p>
-                    결국 아이들을
-                  </p>
-                  <p>
-                    가장 가까이서 만날 수 있는
-                  </p>
-                  <p>
-                    일을 선택하게 되었지만,
-                  </p>
-                  <p>
-                    그 경험들이 결코
-                  </p>
-                  <p>
-                    헛된 것은 아니었어요.
-                  </p>
-                  <br />
-                  <p>
-                    임용 시험을 준비하면서
-                  </p>
-                  <p>
-                    교육학적 지식을
-                  </p>
-                  <p>
-                    더 심화시킬 수 있었고,
-                  </p>
-                  <br />
-                  <p>
-                    영어 유치원에서는
-                  </p>
-                  <p>
-                    4세부터 7세까지
-                  </p>
-                  <p>
-                    어린 아이들을
-                  </p>
-                  <p>
-                    다루는 법을 배웠습니다.
-                  </p>
-                  <br />
-                  <p>
-                    학원에서는
-                  </p>
-                  <p>
-                    초등학생뿐만 아니라
-                  </p>
-                  <p>
-                    학교 내신과 수능 대비
-                  </p>
-                  <p>
-                    수업까지 맡았으며,
-                  </p>
-                  <br />
-                  <p>
-                    교육 회사에서는
-                  </p>
-                  <p>
-                    교재 및 온라인 콘텐츠를
-                  </p>
-                  <p>
-                    제작하는 일을 배웠어요.
-                  </p>
-                  <br />
-                  <p>
-                    이렇게 다양한 현장에서의
-                  </p>
-                  <p>
-                    경험을 토대로
-                  </p>
-                  <p>
-                    저만의 가치가 녹아든
-                  </p>
-                  <p>
-                    다이노 영어를
-                  </p>
-                  <p>
-                    시작하게 되었습니다.
-                  </p>
-                </div>
-              )
-            }
-          </div>
-        </div>
+    <IntroTeacherStyle>
+      <div className="profile-container">
+        <Image
+          src={ImageProfilePicture}
+          className="profile-picture"
+          alt="profile picture"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: '100%', height: 'auto' }}
+        />
       </div>
-    </ArticleStyle>
+      <IntroTeacherSwiper className="intro-teacher-swiper">
+        <Swiper
+          className="profile-swiper"
+          slidesPerView={1}
+          spaceBetween={12}
+          pagination={{
+            clickable: true
+          }}
+          loop={true}
+        >
+          <SwiperSlide>
+            <div className="swiper-teacher first">
+              <div className="swiper-title">
+                1. 미국 초 ・ 중학교 졸업
+              </div>
+              <div className="swiper-content">
+                <p>
+                  저는 초등학교 4학년 때 부모님과 함께 미국으로 갔습니다.
+                </p>
+                <p>
+                  캘리포니아 주, Rancho Cucamonga 라는 작은 동네에서
+                  John L. Golden Elementary School 과
+                  Day Creek Intermediate School 을 졸업했어요.
+                </p>
+                <p>
+                  미국 학교를 다니면서 경험하고 배운 것들을 늘 제 수업에 적용하고 학부모님과 나누고자 노력하고 있습니다.
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiper-teacher second">
+              <div className="swiper-title">
+                2. 영문학 전공 ・ 교직이수
+              </div>
+              <div className="swiper-content">
+                <p>
+                  저는 동국대학교에서 영어영문을 전공하고 교직 과정을 이수했습니다.
+                </p>
+                <p>
+                  대학 생활을 하면서 전공 지식을 쌓는 것도 중요했지만,
+                  무엇보다 값진 경험은 교육 봉사 활동이었어요.
+                  교육 봉사를 하면서 저는 교육이 단지 지식을 전달하는 게 아니라는 걸 알게 되었습니다.
+                </p>
+                <p>
+                  저에게 교육은 사람과 사람 간의 만남이었고,
+                  이러한 만남을 통해 서로의 세계를 이해하고 나누며 더 넓혀갈 수 있다는 것을 배웠어요.
+                </p>
+                <p>
+                  아이들은 저에게 가장 큰 자극과 에너지를 주는 존재입니다.
+                </p>
+                <p>
+                  그리고 아이들에게도 늘 긍정적인 영향을 주는 선생님으로 남고 싶어요.
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiper-teacher third">
+              <div className="swiper-title">
+                3. 다양하고 실질적인 경험
+              </div>
+              <div className="swiper-content">
+                <p>
+                  다이노 영어를 시작하기까지 긴 여정이 있었습니다.
+                </p>
+                <p>
+                  대학을 졸업한 후, 임용 시험 준비부터 영어 유치원과 학원
+                  그리고 사무직까지 다양한 경험을 했습니다.
+                  돌고 돌아 결국 아이들을  가장 가까이서 만날 수 있는 일을 선택하게 되었지만,
+                  그 경험들이 결코 헛된 것은 아니었어요.
+                </p>
+                <p>
+                  임용 시험을 준비하면서 교육학적 지식을 더 심화시킬 수 있었고,
+                  영어 유치원에서는 4세부터 7세까지 어린 아이들을 다루는 법을 배웠습니다.
+                </p>
+                <p>
+                  학원에서는 초등학생뿐만 아니라 학교 내신과 수능 대비
+                  수업까지 맡았으며, 교육 회사에서는 교재 및 온라인 콘텐츠를 제작하는 일을 배웠어요.
+                </p>
+                <p>
+                  이렇게 다양한 현장에서의 경험을 토대로 저만의 가치가 녹아든
+                  다이노 영어를 시작하게 되었습니다.
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </IntroTeacherSwiper>
+    </IntroTeacherStyle>
   )
 }
