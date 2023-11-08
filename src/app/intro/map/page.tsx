93b28e1c5e287b308b0ad9fa6ddbb1fa31d22533
gouz7514 <gouz7514@gmail.com>
2023-11-08 19:02:53 +0900

    Revert "Test : test metadata with generateMetadata"
    This reverts commit 58c39b8db5aee04fbedcdcd5ffc204cd173359ce.

--- src/app/intro/map/page.tsx
+++ src/app/intro/map/page.tsx
@@ -1,13 +1,10 @@
-import { Metadata, ResolvingMetadata } from 'next'
+import { Metadata } from 'next'
 
 import Map from '@/app/components/Page/intro/IntroMap'
 import { METADATA } from '@/lib/constants/constatns'
 import { CustomMetadata } from '@/lib/utils/metadata'
 
-// export const metadata: Metadata = new CustomMetadata(METADATA.introMap)
-export async function generateMetadata() {
-  return new CustomMetadata(METADATA.introMap)
-}
+export const metadata: Metadata = new CustomMetadata(METADATA.introMap)
 
 export default function IntroMap() {
   return (
