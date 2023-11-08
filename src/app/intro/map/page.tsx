58c39b8db5aee04fbedcdcd5ffc204cd173359ce
gouz7514 <gouz7514@gmail.com>
2023-11-08 19:00:08 +0900

    Test : test metadata with generateMetadata

--- src/app/intro/map/page.tsx
+++ src/app/intro/map/page.tsx
@@ -1,10 +1,13 @@
-import { Metadata } from 'next'
+import { Metadata, ResolvingMetadata } from 'next'
 
 import Map from '@/app/components/Page/intro/IntroMap'
 import { METADATA } from '@/lib/constants/constatns'
 import { CustomMetadata } from '@/lib/utils/metadata'
 
-export const metadata: Metadata = new CustomMetadata(METADATA.introMap)
+// export const metadata: Metadata = new CustomMetadata(METADATA.introMap)
+export async function generateMetadata() {
+  return new CustomMetadata(METADATA.introMap)
+}
 
 export default function IntroMap() {
   return (
