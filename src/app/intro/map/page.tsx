66f8a230ce0c18f09c3a182b6c1ee318a2bb276f
gouz7514 <gouz7514@gmail.com>
2023-11-08 19:43:46 +0900

    Test : test metadata with getMetadata

--- src/app/intro/map/page.tsx
+++ src/app/intro/map/page.tsx
@@ -4,15 +4,7 @@ import Map from '@/app/components/Page/intro/IntroMap'
 import { METADATA } from '@/lib/constants/constatns'
 import { CustomMetadata } from '@/lib/utils/metadata'
 
-// export const metadata: Metadata = new CustomMetadata(METADATA.introMap)
-export const metadata: Metadata = {
-  title: '다이노 영어 | 오시는 길',
-  description: '다이노 영어 오시는 길',
-  openGraph: {
-    title: '다이노 영어 | 오시는 길 og',
-    description: '다이노 영어 오시는 길 og',
-  }
-}
+export const metadata: Metadata = new CustomMetadata(METADATA.introMap).getMetadata()
 
 export default function IntroMap() {
   return (
